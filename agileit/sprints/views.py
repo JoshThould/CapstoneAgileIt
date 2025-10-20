07fff6dcd6c450302cc47edb0abd82f501c125e5
JoshThould <jhethould@gmail.com>
2025-10-20 19:54:37 +0100

    Add SprintKanbanView to display sprint details and stories by status

--- agileit/sprints/views.py
+++ agileit/sprints/views.py
@@ -29,3 +29,27 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context['stories_in_progress'] = Story.objects.filter(status='In Progress', owner=self.request.user)
         context['stories_done'] = Story.objects.filter(status='Done', owner=self.request.user)
         return context
+
+# Kanban board view
+class SprintKanbanView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'sprints/kanban.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sprint = get_object_or_404(Sprint, pk=self.kwargs['pk'])
+
+        status_list = [status for status, _ in STATUS_CHOICES]  # ✅ Extract status keys
+
+        context['sprint'] = sprint
+        context['columns'] = [
+            {
+                'status': status,
+                'stories': sprint.stories.filter(status=status, owner=self.request.user)
+            }
+            for status in status_list
+        ]
+        return context
+
+    def test_func(self):
+        sprint = get_object_or_404(Sprint, pk=self.kwargs['pk'])
+        return sprint.owner == self.request.user
