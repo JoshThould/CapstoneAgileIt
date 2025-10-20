0776e7b38f2497d6ac9bb84fdeb31e61e8ef4a79
JoshThould <jhethould@gmail.com>
2025-10-20 23:11:21 +0100

    Register signals in SprintsConfig to ensure they are properly initialized

--- agileit/sprints/apps.py
+++ agileit/sprints/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SprintsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sprints'
+
+
+    def ready(self):
+        import sprints.signals # Import signals to ensure they are registered
