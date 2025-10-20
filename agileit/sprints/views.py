bf58dffa7b9bddee806351ff12fae435d51f04e4
JoshThould <jhethould@gmail.com>
2025-10-20 19:53:10 +0100

    Remove placeholder comment and organize import statements in views.py

--- agileit/sprints/views.py
+++ agileit/sprints/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Sprint, Story, STATUS_CHOICES
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+# Drag and drop functionality
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
