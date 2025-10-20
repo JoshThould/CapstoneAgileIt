79636172ea4203aa9df0455a77a715d513ca60e1
JoshThould <jhethould@gmail.com>
2025-10-20 23:10:39 +0100

    Register Sprint and Story models in the Django admin interface

--- agileit/sprints/admin.py
+++ agileit/sprints/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Sprint, Story
 
 # Register your models here.
+
+@admin.register(Sprint)
+class SprintAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "start_date", "end_date")
+    search_fields = ("title",)
+    ordering = ("start_date",)
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "sprint", "updated_on")
+    list_filter = ("status", "sprint")
+    search_fields = ("title", "description")
+    readonly_fields = ("created_on", "updated_on")
